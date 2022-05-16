e4bee353a37a2e16efa7a8decb51da347d156d70
0721santi <san200526@gmail.com>
2022-05-16 01:25:00 -0500

    Formulario de ingreso completado

--- login/autenticacion.php
+++ login/autenticacion.php
@@ -1,37 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="es" >
 <head>
-    <meta charset="UTF-8">
-    <link href="/../estilo.css" rel="stylesheet">
-    <title>Juegos de Maria</title>
+  <meta charset="UTF-8">
+  <link rel="icon" type="image/png" href="/img/logopjb.png">
+  <title>INGRESO</title>
+  <link href="/css/login.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <div class="LogoInstitucional">
-        <a href="https://pedrojustoberrio.edu.co/"target="_blank"><img src="/img/Logo_Colegio.png" width="230px"></a>
-    </div> 
-    <div class=barra>
-        <h2>Menu Juegos</h2><img src="/img/Icono.png" width="50px">
-        <ul>
-            <li><a href="index.html">Inicio</a></li>
-            <li><a href="#">Menu Juego</a></li>
-            <li><a href="#">Juego 1</a></li>
-            <li><a href="/login/autenticacion.php">Configuración</a></li>
-        </ul>
-    </div>
-    <br><br>
-    <img src="/img/VirgenAlt.png" class=Imagen>
-    <form action="singup-inc.php" method="POST">
-       <input type="text" name="name" placeholder="Nombre Completo..."> 
-       <input type="password" name="pwd" placeholder="Contraseña...">
-       <button type="submit" name="submit">Siguiente</button>
-    </form>
-    <br><br><br><br>
-    <div class="Letra">
-        <p>Bienvenidos</p>
-        <div class="Animacion">
-            <li>Estudiantes</li>
-            <li>Docentes</li>
-        </div>
-    </div>
+	<div class="box-form">
+		<div class="left">
+			<div class="overlay">
+				<img src="/img/VirgenAlt.png">
+			</div>
+		</div>
+		<form action="#modal">
+			<div class="right">
+				<div class="texto">
+					<h5>Ingreso</h5>
+					<div class="inputs">
+						<input type="text" placeholder="Nombre" name="nombre">
+						<br>
+						<input type="password" placeholder="Contraseña" name="contraseña">
+					</div>
+					<br><br>
+					<a href="#modal" class="modal-open"><button>Siguiente</button></a>
+				</div>
+				<div class="imagen">
+					<a href="https://pedrojustoberrio.edu.co/"target="_blank"><img src="/img/Logo_Colegio.png" width="230px"></a>
+				</div>
+			</div>
+		</form>
+	</div>
 </body>
 </html>
