4b5be5e9413ffd8ce375ab79bc260aa69fe52de7
0721santi <san200526@gmail.com>
2022-05-17 19:18:56 -0500

    Se actualizó diseño y contenido de edicion

--- edicion/ndato.php
+++ edicion/ndato.php
@@ -4,20 +4,26 @@
     <title>¿CUANTO CONOCES A MARÍA?</title>
 	<link rel="icon" type="image/png" href="/img/logopjb.png">
     <meta charset="UTF-8">
-    <link href="estilo.css" rel="stylesheet" type="text/css">
+    <link href="/css/estilo.css" rel="stylesheet" type="text/css">
    
 </head>
 <body>
-<div class="/img/LogoInstitucional">
+    <div class=barra>
+        <h2>Menu Juegos</h2><img src="/img/Icono.png" width="50px">
+        <ul>
+            <li><a href="/../index.html">Inicio</a></li>
+            <li><a href="/juego/juego.php">Juego</a></li>
+        </ul>
+    </div>
+    <div class="LogoInstitucional">
             <a href="https://pedrojustoberrio.edu.co/"target="_blank"><img src="/img/Logo_Colegio.png" width="230px"></a>
     </div> 
         <div class=barra>
             <h2>Menu Juegos</h2><img src="/img/Icono.png" width="50px">
             <ul>
                 <li><a href="index.html">Inicio</a></li>
-                <li><a href="#">Menu Juego</a></li>
-                <li><a href="#">Juego 1</a></li>
-                <li><a href="/edicion/ndato">Configuración</a></li>
+                <li><a href="/juego/juego.php">Juego</a></li>
+                <li><a href="/login/autenticacion.php">Inicio de Sesion</a></li>
             </ul>
         </div>
             
@@ -33,7 +39,7 @@
     </form><br><br></div>
     
     <div class="Boton2">
-    <a href="tabla.php"><input type="submit" value="Modificar Preguntas y Respuestas"></a>
+    <a href="/edicion/tabla.php"><input type="submit" value="Modificar Preguntas y Respuestas"></a>
 </div>
 </body>
 </html>
--- edicion/tabla.php
+++ edicion/tabla.php
@@ -4,36 +4,41 @@
     <title>CAMBIO DE DATOS</title>
 	<link rel="icon" type="image/png" href="/img/logopjb.png">
     <meta charset="UTF-8">
-    <link href="estilo.css" rel="stylesheet" type="text/css">
-    <style>
-        a{
-            text-decoration: none;
-        }
-    </style>
+    <link href="/css/estilo.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<table>
-        <tr>
-            <thead>
-                <th>ID PREGUNTA</th>
-                <th>PREGUNTA</th>
-                <th>DIFICULTAD</th>
-            </thead>
-        </tr>
-        <?php
-            $conexion = mysqli_connect("localhost","root","juan1");
+    <div class=barra>
+        <h2>Menu Juegos</h2><img src="/img/Icono.png" width="50px">
+        <ul>
+            <li><a href="/../index.html">Inicio</a></li>
+            <li><a href="/juego/juego.php">Juego</a></li>
+        </ul>
+    </div>
+    <div class=Tabla>
+        <table>
+            <tr>
+                <thead>
+                    <th>ID PREGUNTA</th>
+                    <th>PREGUNTA</th>
+                    <th>DIFICULTAD</th>
+                </thead>
+            </tr>
+            <?php
+                $conexion = mysqli_connect("localhost","root","juan1");
 
-            //if (!$conexion) { echo "Conexión fallida<br>"; } else { echo "Conectado exitosamente<br>"; }
+                //if (!$conexion) { echo "Conexión fallida<br>"; } else { echo "Conectado exitosamente<br>"; }
 
-            $consulta = mysqli_query($conexion, "SELECT * FROM sakila.daticos");
-            echo "<form active='reemplazo.php'>";
-            while ($fila = mysqli_fetch_assoc($consulta))
-            {echo "<tr><td><a href='confirmacion.php?iddaticos=".$fila["iddaticos"]."&preg=".$fila["preguntas"]."&resp=".$fila["dificultad"]."'>".$fila["iddaticos"]."</td><td><a href='cambios.php?iddaticos=".$fila["iddaticos"]."&preg=".$fila["preguntas"]."&resp=".$fila["dificultad"]."'>".$fila["preguntas"]."</a></td><td><a href='cambios.php?iddaticos=".$fila["iddaticos"]."&preg=".$fila["preguntas"]."&resp=".$fila["dificultad"]."'>".$fila["dificultad"]."</a></td></tr>";}
-            echo "</form>";
-            mysqli_close($conexion);
-        ?>
-
-    </table>
-    <a href="/edicion/ndato.php"><h1>VOLVER A PÁGINA DE INGRESO DE DATOS</h1></a>
+                $consulta = mysqli_query($conexion, "SELECT * FROM preguntasmaria.preguntas");
+                echo "<form active='reemplazo.php'>";
+                while ($fila = mysqli_fetch_assoc($consulta))
+                {echo "<tr><td><a href='confirmacion.php?iddaticos=".$fila["id"]."&preg=".$fila["pregunta"]."&resp=".$fila["dificultad"]."'>".$fila["id"]."</td><td><a href='cambios.php?iddaticos=".$fila["id"]."&preg=".$fila["pregunta"]."&resp=".$fila["dificultad"]."'>".$fila["pregunta"]."</a></td><td><a href='cambios.php?iddaticos=".$fila["id"]."&preg=".$fila["pregunta"]."&resp=".$fila["dificultad"]."'>".$fila["dificultad"]."</a></td></tr>";}
+                echo "</form>";
+                mysqli_close($conexion);
+            ?>
+        </table>
+    </div>
+    <div class=Volver>
+        <a href="/edicion/ndato.php" ><input type="submit" value="VOLVER A PÁGINA DE INGRESO DE DATOS"></a>
+    </div>
 </body>
 </html>
