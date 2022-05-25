167b577f2bf0ebefb81c85ef921a870e259719e6
0721santi <san200526@gmail.com>
2022-05-24 23:20:53 -0500

    onclick -- +1

--- juego/juegopri.php
+++ juego/juegopri.php
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <?php 
-        $nom = $_GET['user'];
-        $pass = $_GET['pass'];
-   ?> 
-</body>
-=======
 <html>
 <?php
     $counter = 0;
+    $nom = $_GET['user'];
+    $pass = $_GET['pass'];
 ?>
 <head>
     <meta charset="UTF-8">
     <title>Contador</title>
 </head>
 <body>
-    <button onclick=<?php $counter + 1 ?>><?php echo $counter ?></button>
+    <button onclick="<?php $counter=$counter+1 ?>"><?php echo $counter ?></button>
 </body>
 
->>>>>>> bb29b450be092873cd9a1dd8bda4fe06a3c252b4
 </html>
