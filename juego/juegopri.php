88a31f415cbcd7877e86c785f5d13a16b80969d8
Francisco Gonzalez <frangc0206@gmail.com>
2022-05-25 01:07:36 -0300

    Implemented Counter

---
+++ juego/juegopri.php
@@ -0,0 +1,13 @@
+<html>
+<?php
+    $counter = 0;
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>Contador</title>
+</head>
+<body>
+    <button onclick=<?php $counter + 1 ?>><?php echo $counter ?></button>
+</body>
+
+</html>
